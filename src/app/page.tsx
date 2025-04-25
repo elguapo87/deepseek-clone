ec52ee977b57f6134a573bb588339e270eefb4c0
elguapo87 <pglogovac@gmail.com>
2025-04-26 01:19:35 +0200

    Added states in page.tsx ( HomePage ) & added classNames

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,46 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { assets } from "../../assets/assets";
+
 export default function Home() {
+
+  const [expand, setExpand] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <div >
-      Cigareta
+    <div>
+      <div className="flex h-screen">
+        {/* SIDEBAR */}
+        <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#29222d] text-white relative">
+          <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
+            <Image onClick={() => setExpand(prev => !prev)} src={assets.menu_icon} alt="" className="rotate-180"  />
+            <Image src={assets.chat_icon} alt="" className="opacity-70"  />
+          </div>
+
+          {
+            messages.length === 0
+                  ?
+                (<>
+                  <div className="flex items-center gap-3">
+                    <Image src={assets.logo_icon} alt="" className="h-16" />
+                    <p className="text-2xl font-medium">Hi, I'm DeepSeek</p>
+                  </div>
+                  <p className="text-sm mt-2">How can I help you today?</p>
+                </>)
+                  :
+               (<div>
+
+               </div>)
+          }
+
+          {/* PROMPT BOX */}
+          <p className="text-xs absolute bottom-1 text-gray-500">AI generated, for refrence only</p>
+        </div>
+
+      </div>
     </div>
   );
 }
