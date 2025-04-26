81bae286d62ca013fcc971948036e41342ca7ae4
elguapo87 <pglogovac@gmail.com>
2025-04-26 03:15:13 +0200

    Added Sidebar component, included sidebar in HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,20 @@
 import Image from "next/image";
 import { useState } from "react";
 import { assets } from "../../assets/assets";
+import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
 
-  const [expand, setExpand] = useState(false);
+  const [expand, setExpand] = useState<boolean>(false);
   const [messages, setMessages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   return (
     <div>
       <div className="flex h-screen">
-        {/* SIDEBAR */}
+        
+        <Sidebar expand={expand} setExpand={setExpand} />
+
         <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#29222d] text-white relative">
           <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
             <Image onClick={() => setExpand(prev => !prev)} src={assets.menu_icon} alt="" className="rotate-180"  />
