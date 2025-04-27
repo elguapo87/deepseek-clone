aaacc1b515b4075097c23986e18a514fc0814b42
elguapo87 <pglogovac@gmail.com>
2025-04-27 23:12:10 +0200

    Created configuraton file for connecting project to mongodb

---
+++ src/config/db.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+type MongooseConnection = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
+declare global {
+  var mongoose: MongooseConnection | undefined;
+}
+
+const cached: MongooseConnection = global.mongoose ?? { conn: null, promise: null };
+
+export default async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    if (!process.env.MONGODB_URI) {
+      throw new Error("MONGODB_URI is not defined in environment variables");
+    }
+    cached.promise = mongoose.connect(process.env.MONGODB_URI);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+    global.mongoose = cached;
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+    throw error;
+  }
+
+  return cached.conn;
+}
