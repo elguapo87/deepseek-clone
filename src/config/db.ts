46c5912fdf08cefb544240a280ac1b7d83c42739
elguapo87 <pglogovac@gmail.com>
2025-04-28 02:02:38 +0200

    Fixed connectDB function for vercel deployment

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,33 +1,18 @@
 import mongoose from "mongoose";
 
-type MongooseConnection = {
-  conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
+let isConnected = false;
+
+const connectDB = async () => {
+    if (isConnected) return; // Prevent reconnecting
+
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}`);
+        isConnected = true;
+        console.log("Database Connected");
+    } catch (error) {
+        console.error("Connection Failed", error);
+        process.exit(1);
+    }
 };
 
-declare global {
-  var mongoose: MongooseConnection | undefined; // <-- Must use var here
-}
-
-const cached = global.mongoose ?? { conn: null, promise: null };
-
-export default async function connectDB() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    if (!process.env.MONGODB_URI) {
-      throw new Error("MONGODB_URI is not defined in environment variables");
-    }
-    cached.promise = mongoose.connect(process.env.MONGODB_URI);
-  }
-
-  try {
-    cached.conn = await cached.promise;
-    global.mongoose = cached;
-  } catch (error) {
-    console.error("Error connecting to MongoDB", error);
-    throw error; // important to throw error here
-  }
-
-  return cached.conn;
-}
+export default connectDB;
