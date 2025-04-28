a2a4ba86b8d83fb54f6bab0c3a5935504fc228c6
elguapo87 <pglogovac@gmail.com>
2025-04-28 20:35:52 +0200

    Fixed clerk route for deploying app on vercel

--- src/app/api/clerk/route.ts
+++ src/app/api/clerk/route.ts
@@ -1,12 +1,11 @@
 import { Webhook } from "svix";
 import connectDB from "@/config/db";
 import userModel from "@/models/userModel";
-import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 interface ClerkUser {
   id: string;
-  first_name: string;
+  first_name: string | null;
   last_name: string | null;
   email_addresses: { email_address: string }[];
   image_url?: string;
@@ -22,21 +21,22 @@ export async function POST(req: NextRequest) {
   if (!signinSecret) throw new Error("signinSecret is not defined");
 
   const wh = new Webhook(signinSecret);
-  const headerPayload = headers();
+
+  // ⛔ DON'T USE next/headers() here
   const svixHeaders = {
-    "svix-id": (await headerPayload).get("svix-id") ?? "",
-    "svix-signature": (await headerPayload).get("svix-signature") ?? "",
+    "svix-id": req.headers.get("svix-id") ?? "",
+    "svix-signature": req.headers.get("svix-signature") ?? "",
   };
 
   try {
-    const body = await req.text(); 
+    const body = await req.text(); // 🛠️ Important to use text()
     const { data, type } = wh.verify(body, svixHeaders) as ClerkEvent;
 
     const userData = {
       _id: data.id,
       email: data.email_addresses[0].email_address,
-      name: `${data.first_name} ${data.last_name || ""}`,
-      image: data.image_url || "",
+      name: `${data.first_name ?? ""} ${data.last_name ?? ""}`,
+      image: data.image_url ?? "",
     };
 
     await connectDB();
@@ -52,6 +52,7 @@ export async function POST(req: NextRequest) {
         await userModel.findByIdAndDelete(data.id);
         break;
       default:
+        console.log(`Unhandled event type: ${type}`);
         break;
     }
 
