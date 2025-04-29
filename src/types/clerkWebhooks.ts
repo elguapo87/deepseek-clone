e3a2df1a9b26b7e518e1cd4bca2f0b9643250566
elguapo87 <pglogovac@gmail.com>
2025-04-29 02:32:36 +0200

    Reverted files from js to ts & fixed connectionDB & clerk webhooks route

--- src/types/clerkWebhooks.ts
+++ src/types/clerkWebhooks.ts
@@ -1,21 +1,20 @@
 // types/clerkWebhooks.ts
 export interface ClerkWebhookEventData {
-    id: string;
-    email_addresses: { email_address: string }[];
-    first_name?: string;
-    last_name?: string;
-    image_url?: string;
-    object: string; // Can be "user"
-  }
-  
-  export interface ClerkWebhookEvent {
-    type: "user.created" | "user.updated" | "user.deleted";
-    data: ClerkWebhookEventData;
-    event_attributes?: {
-      http_request?: {
-        client_ip: string;
-        user_agent: string;
-      };
+  id: string;
+  email_addresses: { email_address: string }[];
+  first_name?: string;
+  last_name?: string;
+  image_url?: string;
+  object: string; // Can be "user"
+}
+
+export interface ClerkWebhookEvent {
+  type: "user.created" | "user.updated" | "user.deleted";
+  data: ClerkWebhookEventData;
+  event_attributes?: {
+    http_request?: {
+      client_ip: string;
+      user_agent: string;
     };
-  }
-  
+  };
+}
