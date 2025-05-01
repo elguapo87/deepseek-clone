0286f18dedebd4704cb889580d88e4f164307dc5
elguapo87 <pglogovac@gmail.com>
2025-05-01 14:41:52 +0200

    Added dynamic data in HomePage & in Message component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,49 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { assets } from "../../assets/assets";
 import Sidebar from "@/components/Sidebar";
 import PromptBox from "@/components/PromptBox";
 import Message from "@/components/Message";
+import { AppContext } from "@/context/AppContext";
+
+type MessageType = {
+  _id: number;
+  role: string;
+  content: string;
+  timestamp: number;
+};
 
 export default function Home() {
 
+  const context = useContext(AppContext);
+  if (!context) throw new Error("HomePage must be within AppContextProvider");
+  const { selectedChat } = context;
+;
   const [expand, setExpand] = useState<boolean>(false);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<MessageType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (selectedChat) {
+      setMessages(selectedChat.messages);
+    }
+  }, [selectedChat]);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTo({
+        top: containerRef.current.scrollHeight,
+        behavior: "smooth"
+      })
+    }
+  }, [messages]);
+
+  console.log(selectedChat);
+  
 
   return (
     <div>
@@ -37,8 +68,23 @@ export default function Home() {
                   <p className="text-sm mt-2">How can I help you today?</p>
                 </>)
                   :
-               (<div>
-                  <Message role="user" content="What is Next JS?" />
+               (<div ref={containerRef} className="relative flex flex-col items-center justify-start w-full mt-20 max-h-screen overflow-y-auto">
+                  <p className="fixed top-8 border border-transparent hover:border-gray-500/50 px-2 py-1 rounded-lg font-semibold mb-6">{selectedChat.name}</p>
+
+                  {messages.map((msg) => (
+                    <Message key={msg._id} role={msg.role} content={msg.content} />
+                  ))}
+
+                  {isLoading && (
+                    <div className="flex gap-4 max-w-3xl w-full py-3">
+                      <Image src={assets.logo_icon} alt="Logo" className="h-9 w-9 p-1 border border-white/15 rounded-full" />
+                      <div className="loader flex justify-center items-center gap-1">
+                        <div className="w-1 h-1 rounded-full bg-white animate-bounce"></div>
+                        <div className="w-1 h-1 rounded-full bg-white animate-bounce"></div>
+                        <div className="w-1 h-1 rounded-full bg-white animate-bounce"></div>
+                      </div>
+                    </div>
+                  )}
                </div>)
           }
 
